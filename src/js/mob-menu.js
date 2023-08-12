6acbdd1382a1b7d4bc46d61b173453fcf76d3bdb
Oleg9145 <oleg.zinenko94@gmail.com>
2023-08-12 12:36:24 +0200

    Мобільна і планшет хедер і хіро

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,28 +1,27 @@
 (() => {
-    const refs = {
-      menuToggle: document.querySelector("[data-menu-toggle]"),
-      Menu: document.querySelector("[data-menu]"),
-    };
-  
-    refs.menuToggle.addEventListener("click", toggleMenu);
-  
-    function toggleMenu() {
-      const targetMenu = refs.menuToggle.getAttribute('data-menu-target');
-  
-      if (refs.Menu.classList.contains("is-hidden")) {
-        // Відкриття меню
-        refs.Menu.classList.remove("is-hidden");
-        document.body.classList.add('no-scroll');
-        refs.Menu.classList.add("slide-out-right");
-        // Змінити іконку на хрестик
-        refs.menuToggle.querySelector("use").setAttribute("href", "../img/svg-icon.svg#icon-close");
-      } else {
-        // Закриття меню без анімації
-        refs.Menu.classList.add("is-hidden");
-        document.body.classList.remove('no-scroll');
-        // Змінити іконку на бургер
-        refs.menuToggle.querySelector("use").setAttribute("href", "./img/svg-icon.svg#icon-burger");
-      }
+  const refs = {
+    menuToggle: document.querySelector("[data-menu-toggle]"),
+    Menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.menuToggle.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    const targetMenu = refs.menuToggle.getAttribute('data-menu-target');
+
+    if (refs.Menu.classList.contains("is-hidden")) {
+      // Відкриття меню
+      refs.Menu.classList.remove("is-hidden");
+      document.body.classList.add('no-scroll');
+      refs.Menu.classList.add("slide-out-right");
+      // Змінити іконку на хрестик
+      refs.menuToggle.querySelector("use").setAttribute("href", "../img/svg-icon.svg#icon-close");
+    } else {
+      // Закриття меню без анімації
+      refs.Menu.classList.add("is-hidden");
+      document.body.classList.remove('no-scroll');
+      // Змінити іконку на бургер
+      refs.menuToggle.querySelector("use").setAttribute("href", "../img/svg-icon.svg#icon-burger");
     }
-  })();
-  
+  }
+})();
