af3d3f8ef0744c6026bccbeb2311097740118793
NatHumeniuk <nat.gumenuk@gmail.com>
2023-08-14 02:14:37 +0300

    general correction

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,27 +1,31 @@
 (() => {
   const refs = {
-    menuToggle: document.querySelector("[data-menu-toggle]"),
-    Menu: document.querySelector("[data-menu]"),
+    menuToggle: document.querySelector('[data-menu-toggle]'),
+    Menu: document.querySelector('[data-menu]'),
   };
 
-  refs.menuToggle.addEventListener("click", toggleMenu);
+  refs.menuToggle.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     const targetMenu = refs.menuToggle.getAttribute('data-menu-target');
 
-    if (refs.Menu.classList.contains("is-hidden")) {
+    if (refs.Menu.classList.contains('is-hidden')) {
       // Відкриття меню
-      refs.Menu.classList.remove("is-hidden");
+      refs.Menu.classList.remove('is-hidden');
       document.body.classList.add('no-scroll');
-      refs.Menu.classList.add("slide-out-right");
+      refs.Menu.classList.add('slide-out-right');
       // Змінити іконку на хрестик
-      refs.menuToggle.querySelector("use").setAttribute("href", "../img/svg-icon.svg#icon-close");
+      refs.menuToggle
+        .querySelector('use')
+        .setAttribute('href', './img/svg-icon.svg#icon-close');
     } else {
       // Закриття меню без анімації
-      refs.Menu.classList.add("is-hidden");
+      refs.Menu.classList.add('is-hidden');
       document.body.classList.remove('no-scroll');
       // Змінити іконку на бургер
-      refs.menuToggle.querySelector("use").setAttribute("href", "../img/svg-icon.svg#icon-burger");
+      refs.menuToggle
+        .querySelector('use')
+        .setAttribute('href', './img/svg-icon.svg#icon-burger');
     }
   }
 })();
